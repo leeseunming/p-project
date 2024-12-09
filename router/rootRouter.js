a9297deb7c3edadc2c4f53d051772ef0c9b50e35
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2024-12-10 07:14:57 +0900

    이벤트 페이지 작성 & css 스타일 수정

--- router/rootRouter.js
+++ router/rootRouter.js
@@ -7,4 +7,14 @@ router.get('/', (req, res) => {
     root.home(req, res); // 메인 페이지 처리
 });
 
+// twenty 페이지 처리
+router.get('/twenty', (req, res) => {
+    root.twenty(req, res); // twenty 페이지 처리
+});
+
+// 검색 처리
+router.post('/search', (req, res) => {
+    root.search(req, res);
+});
+
 module.exports = router;
